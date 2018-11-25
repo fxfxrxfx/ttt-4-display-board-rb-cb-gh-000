e7b7ffeaf64c3b36745c2f73fca3b246ffee923c
Santiago Betancur <santi-c-b@outlook.com>
2018-11-25 05:18:52 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(boardArr)
+  puts " #{boardArr[0]} | #{boardArr[1]} | #{boardArr[2]} "
+  puts "-----------"
+  puts " #{boardArr[3]} | #{boardArr[4]} | #{boardArr[5]} "
+  puts "-----------"
+  puts " #{boardArr[6]} | #{boardArr[7]} | #{boardArr[8]} "
+end
